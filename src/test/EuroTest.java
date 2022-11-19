d508f1ae3a4f97803015672e4e052f7ce8edca64
Lorenzo <lorenzo.cappello.majo@gmail.com>
2022-11-19 14:16:33 +0100

    inserimento setup

--- src/test/EuroTest.java
+++ src/test/EuroTest.java
@@ -3,52 +3,55 @@ package test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import code.Buisness_logic.Euro;
 
 public class EuroTest
  {
+    private Euro e;
+
+    @Before
+    public void setup()
+    {
+        e=new Euro(20);
+    }
 
     @Test
     public void testGetValore() 
     {
-        Euro e=new Euro(20);
         assertEquals(20,e.getValore()/100);
     }
 
     @Test
     public void testMinoreDi() 
     {
-        Euro e=new Euro(20);
         assertTrue(e.minoreDi(new Euro(30)));
     }
 
     @Test
     public void testSomma() 
     {
-        Euro e=new Euro(20);
-        assertEquals(20,e.getValore());
+        Euro temp=e.somma(e);
+        assertEquals(40,temp.getValore()/100);
     }
 
     @Test
     public void testSottrai() 
     {
-        Euro e=new Euro(20);
-        assertEquals(20,e.getValore());
+ 
     }
 
     @Test
     public void testStampa() 
     {
-        Euro e=new Euro(20);
-        assertEquals(20,e.getValore());
+
     }
 
     @Test
     public void testUgualeA() 
     {
-        Euro e=new Euro(20);
-        assertEquals(20,e.getValore());
+  
     }
 }
